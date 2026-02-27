ee96796ab04db475719d35a14e6856690ed40751
Abdallahtrd <abdallahnura447@gmail.com>
2026-02-27 15:53:41 +0100

    Initial working calculator with processor and collector functions.

---
+++ README.md
@@ -0,0 +1,16 @@
+# CLI CALCULATOR.
+
+## DESCRIPTION
+This is a simple command-line calculator written in python. It allows users to input numbers and perform basic arithmethic operations.
+
+
+## PURPOSE
+This is project #2 from the projects i am building to understand and master the basic logic
+in python.
+
+## FEATURES PLANED
+Collect numbers from user
+Choose an operation (+,-,*,/)
+Display the result
+Handle invalid input safely
+
---
+++ calculator.py
@@ -0,0 +1,46 @@
+def collect_number(): #This function collects the number from the user
+    while True:
+        try:
+            number = float(input("Input number "))
+            return number
+        except ValueError:
+            print("Invalid input!!!")
+
+def operation_list():
+    print("Your operators are..")
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    
+def collect_operation():
+    while True:
+        operator = input("PICK AN OPERATOR ")
+        return operator
+
+def calculation(first_number, second_number, operator):
+
+        if operator == '+':
+            return first_number + second_number
+        elif operator == '-':
+            return first_number - second_number
+    
+        elif operator == '*':
+            return first_number * second_number
+    
+        elif operator == '/':
+            return first_number / second_number
+        else:
+             print("Invalid operation")
+
+def main():
+
+    first_number = collect_number()
+    second_number = collect_number()
+    operation_list()
+    operator = collect_operation()
+    result = calculation(first_number, second_number, operator)
+    print(result)
+
+
+main()
